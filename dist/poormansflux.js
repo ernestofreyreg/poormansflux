7dda96f273ef2a84ffb64241a0d24496cacd458e
Ernesto Freyre Gonzalez <ernestofreyreg@gmail.com>
2015-09-15 12:12:04 -0700

    fixed small deps

---
+++ dist/poormansflux.js
@@ -0,0 +1,54 @@
+/**
+ *  Copyright (c) 2014-2015, Ernesto Freyre G.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    // Node, CommonJS-like
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    root.poorMansFluxMixin = factory();
+  }
+}(this, function () {
+  return function(store, actions) {
+    return {
+      getInitialState: function() {
+        return store;
+      },
+      childContextTypes: {
+        flux: React.PropTypes.object
+      },
+
+      getChildContext: function() {
+        var
+          flux;
+
+        flux = { store: this.state };
+        flux.actions = actions(this.dispatch, flux);
+
+        return {
+          flux: flux
+        };
+      },
+
+      dispatch: function(data) {
+        this.setState(data);
+      }
+    };
+  };
+}));
+
+
+
+
+
+
+
+
