c49a7054f4e2f3165fdf58e1a9c905470331d122
Ernesto Freyre Gonzalez <ernestofreyreg@gmail.com>
2015-09-14 18:40:31 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### Poor Man's Flux
+
+A way to simple implementation of Flux, embeded on React components.
+
---
+++ poormansflux.js
@@ -0,0 +1,114 @@
+'use strict';
+
+
+var
+  fluxComponentMixin,
+  Button,
+  App,
+  myActionsSimpler,
+  myActions;
+
+
+fluxComponentMixin = function(store, actions) {
+  return {
+    getInitialState: function() {
+      return store;
+    },
+    childContextTypes: {
+      flux: React.PropTypes.object
+    },
+
+    getChildContext: function() {
+      var
+        flux;
+
+      flux = { store: this.state };
+      flux.actions = actions(this.dispatch, flux);
+
+      return {
+        flux: flux
+      };
+    },
+
+    dispatch: function(data) {
+      this.setState(data);
+    }
+  };
+};
+
+Button = React.createClass({
+  propTypes: {
+    pressed: React.PropTypes.number.isRequired
+  },
+
+  render: function() {
+    return (<button className="btn btn-default" onClick={this.increase}>{this.props.pressed} {this.props.children}</button>);
+  },
+
+  increase: function(evt) {
+    if (this.props.doA) {
+      this.context.flux.actions.increaseNumberA();
+    }
+
+    if (this.props.doB) {
+      this.context.flux.actions.increaseNumberB();
+    }
+
+    evt.preventDefault();
+  },
+
+  contextTypes: {
+    flux: React.PropTypes.object.isRequired
+  }
+});
+
+myActions = function(dispatch, flux) {
+  var
+    actions,
+    getOrInit;
+
+  getOrInit = function() {
+    if (!actions) {
+      actions = {
+        increaseNumberA: function() {
+          dispatch({valueA: flux.store.valueA + 1});
+        },
+        increaseNumberB: function() {
+          dispatch({valueB: flux.store.valueB + 1});
+        }
+      };
+    }
+
+    return actions;
+  };
+
+  return getOrInit;
+};
+
+
+myActionsSimpler = function(dispatch, flux) {
+  return {
+    increaseNumberA: function() {
+      dispatch({valueA: flux.store.valueA + 1});
+    },
+    increaseNumberB: function() {
+      dispatch({valueB: flux.store.valueB + 1});
+    }
+  };
+};
+
+
+App = React.createClass({
+  mixins: [fluxComponentMixin({valueA: 0, valueB: 0}, myActionsSimpler)],
+
+  render: function() {
+    return (
+      <div>
+        <Button pressed={this.state.valueA} doA>Button A</Button>
+        <Button pressed={this.state.valueB} doB>Button B</Button>
+      </div>
+    );
+  }
+});
+
+React.render( <App/>, document.getElementById('container'));
